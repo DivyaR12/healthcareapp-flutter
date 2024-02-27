253155fb348196d61dd5c69d5e32c12dc01c111b
Divya Ramesh <divya.rameshh1216@gmail.com>
2024-02-27 03:11:23 +0000

    changes to files

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+class Config {
+  static const String appName = "HeathcareApp";
+  static const String apiURL = '127.0.0.1:4000'; //PROD_URL
+  static const loginAPI = "/login";
+  static const registerAPI = "/register";
+  static const userProfileAPI = "/user-Profile";
+}
